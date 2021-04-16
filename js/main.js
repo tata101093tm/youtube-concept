0d77e2128331685ff02e12e43289beac26441211
tata101093tm <82052023+tata101093tm@users.noreply.github.com>
2021-04-16 15:08:34 +0700

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -2,7 +2,26 @@ const swiper = new Swiper('.channel-slider', {
   // Optional parameters
  
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+       slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+   1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+    
+  },
 
 
 
@@ -14,3 +33,80 @@ const swiper = new Swiper('.channel-slider', {
 
 
 });
+
+const swiper2 = new Swiper('.recommended-slider', {
+  // Optional parameters
+ 
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+       slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    },
+  },
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+
+
+});
+
+const swiper3 = new Swiper('.channel-recommended-channel-slider', {
+  // Optional parameters
+ 
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+       slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+   1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-recommended-channel-button-next',
+    prevEl: '.channel-recommended-channel-button-prev',
+  },
+
+
+});
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy(); 
+  swiper2.destroy();
+  swiper3.destroy();
+}
+
+
+
+
+
